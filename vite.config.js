a1d7a75c6829ed7c467ee2a0e4cb399c8823974d
jane <tth17598@gmail.com>
2024-08-23 16:24:33 +0700

    Fix: build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable no-undef */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/old-polaris-icon/'
+  define: {
+    'process.env': {},
+  },
+  build: {
+    cssCodeSplit: true,
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.jsx'),
+      name: 'polaris-icon',
+      fileName: () => 'app.js',
+      formats: ['umd'],
+    },
+  },
 })
